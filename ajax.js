820bde54b58d743908414cddeaab7d4b9f477bb0
luungoc2005 <luungoc2005@gmail.com>
2016-11-16 22:46:52 +0700

    Dividing into multiple classes

--- ajax.js
+++ ajax.js
@@ -1,51 +1,45 @@
-(function ($) {
+(function (global, controls, spotify, $) {
 	var spotifyUri = {
 		search: "https://api.spotify.com/v1/search"
-	}
-	
-	var defaults = {
-		max_results: 10
-	}
+	}	
 	
 	var currentResults;
 	var currentArtist;
 	
 	var searchFor = function (query) {
 		if (query == null) return;
-		if (query == "") $("#searchResults").empty();
+		if (query == "") $(markups.results_box).empty();
 		
 		$.getJSON(spotifyUri.search + "?q=" + encodeURIComponent(query) + "&type=artist", 
 			function(result) {
 				if (result == null) return;
-				$("#searchResults").empty();
+				$(markups.results_box).empty();
 				
 				currentResults = result["artists"]["items"] || currentResults;
 				
-				addSearchResults();
+				spotify.addSearchResults();
 			});		
 	};
 	
-	var addSearchResults = function() {
+	spotify.addSearchResults = function() {
 		$.each(currentResults, function(index, value) {
 			if (index + 1 > defaults.max_results) return false;
 			
-			$("<div />", {
-				"text": value["name"]
-			}).appendTo($("#searchResults"));
+			controls.addSearchResultItem(value["name"]);
 		});
 	}
 	
-	var setCurrentArtist = function (artist) {
-		
+	spotify.setCurrentArtist = function (artist) {
+		currentArtist = artist;
 	}
 	
-	var gotoResult = function() {
+	spotify.gotoResult = function() {
 		if (currentResults != null && currentResults.length > 0) setCurrentArtist(currentResults[0]);
 	}
 	
-	init = function() {
-		$("#searchBox").on("change paste keyup", function () {
-			searchFor($("#searchBox").val());
+	spotify.init = function() {
+		$(markups.search_box).on("change paste keyup", function () {
+			searchFor($(markups.search_box).val());
 		});
 	};
-})(jQuery);
+})(global, controls, spotify, jQuery);
